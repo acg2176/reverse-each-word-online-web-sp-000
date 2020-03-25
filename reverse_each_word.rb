def2c15e60dd1eb2164f230f0809b901c0117f69
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 20:50:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split()
   new_array = []
   new_sentence.each do |word|
+    word = ""
     word.each_char do |char|
       word = char + word
     end
