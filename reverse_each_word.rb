627185db77b948bd98e250cc48c6b69b4c7d2624
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 20:53:02 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,8 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split()
   new_array = []
   new_sentence.each do |word|
-    word = ""
-    word.each_char do |char|
-      word = char + word
-    end
+    word.reverse()
     new_array << word
   end
+  new_array.join
 end
